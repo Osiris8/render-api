441d9d5e01173812841b201f151a47c3374af873
Osiris8 <miganosiris8@gmaaail.com>
2023-10-23 15:33:01 +0200

    add mongoDB logic connection

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+require("dotenv").config({ path: ".env.local" });
 
 app.use(cors()); // Active CORS
 const corsOptions = {
@@ -14,8 +15,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
-require("dotenv").config();
 
 // Middleware pour le message de bienvenue personnalisé
 function messageMiddleware(req, res, next) {
@@ -34,3 +33,30 @@ app.get("/", messageMiddleware);
 app.listen(port, () => {
   console.log(`Le serveur est en cours d'exécution sur le port ${port}`);
 });
+
+/*****************MONGO DB */
+// Import the mongoose module
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+// Define the database URL to connect to.
+const mongoDB =
+  "mongodb+srv://" +
+  process.env.MONGODB_USERNAME +
+  ":" +
+  process.env.MONGODB_PASSWORD +
+  "@cluster0.z3a2i.mongodb.net/source";
+
+// Wait for database to connect, logging an error if there is a problem
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB).then(() => {
+    console.log("Connected to MongoDB");
+  });
+  try {
+    await mongoose.connect(mongoDB);
+  } catch (err) {
+    console.log(err);
+  }
+}
